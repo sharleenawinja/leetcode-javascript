65a7bcded85593a08f173d5107b39e60d11e1ab4
sharleenawinja <awinjasharleen@gmail.com>
2023-04-12 16:48:12 +0300

    check if instance

--- isInstance.js
+++ isInstance.js
@@ -1,1 +1,23 @@
 // check if object is an instance of a class
+
+function checkIfInstance(objectName, className) {
+  // The instanceof operator tests to see if the prototype property of a constructor appears anywhere in the prototype chain of an object. The return value is a boolean value.
+
+  return objectName instanceof className;
+}
+
+class Animal {
+  constructor(sound) {
+    this.name = sound;
+  }
+}
+
+const dog = new Animal("bark");
+
+const testObject = {
+  property: "testProperty",
+  secondProperty: "second test property",
+};
+
+console.log(checkIfInstance(dog, Animal));
+console.log(checkIfInstance(testObject, Animal));
