bc60e1babf5d85792171b9c01bfae2acd766559b
sharleenawinja <awinjasharleen@gmail.com>
2023-04-12 17:12:14 +0300

    second method for last element

--- arraylast.js
+++ arraylast.js
@@ -19,3 +19,18 @@ const optimized = new OptimizeArray([1, 2, 3, 4]);
 const emptyArr = new OptimizeArray([]);
 console.log(optimized.lastElement());
 console.log(emptyArr.lastElement());
+
+// second method
+Array.prototype.last = function () {
+  if (this.length === 0) {
+    return -1;
+  } else {
+    return this[this.length - 1];
+  }
+};
+
+const testArray = [67, 6678, 5678];
+console.log(testArray.last());
+
+const testEmpty = [];
+console.log(testEmpty.last());
