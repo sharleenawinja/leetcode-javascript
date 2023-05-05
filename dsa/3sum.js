df99f8b0d3f6984cc9cd21f27e2606f8f4691f80
sharleenawinja <awinjasharleen@gmail.com>
2023-05-05 21:15:07 +0300

    3sum

--- dsa/3sum.js
+++ dsa/3sum.js
@@ -16,3 +16,27 @@ function threeSumClosest(nums, target) {
   }
   return result;
 }
+
+function threeSumClosest(nums, target) {
+  nums.sort((a, b) => a - b);
+  let result = Infinity;
+  let minDiff = Infinity;
+  for (let i = 0; i < nums.length - 2; i++) {
+    let j = i + 1;
+    let k = nums.length - 1;
+    while (j < k) {
+      const sum = nums[i] + nums[j] + nums[k];
+      const diff = Math.abs(sum - target);
+      if (diff < minDiff) {
+        minDiff = diff;
+        result = sum;
+      }
+      if (sum < target) {
+        j++;
+      } else {
+        k--;
+      }
+    }
+  }
+  return result;
+}
