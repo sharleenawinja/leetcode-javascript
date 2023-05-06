2c05c56509bea6937f09a366564f3fbbee3a195b
sharleenawinja <awinjasharleen@gmail.com>
2023-05-06 23:21:47 +0300

    array.reduce

--- dsa/searchPosition.js
+++ dsa/searchPosition.js
@@ -1,3 +1,6 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found.
+// If not, return the index where it would be if it were inserted in order.
+
 function searchInsert(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] >= target) {
@@ -41,4 +44,15 @@ function searchInsert(nums, target) {
 
 function searchInsert(nums, target) {
   return nums.findIndex((num) => num >= target);
+}
+
+function searchInsert(nums, target) {
+  return (
+    nums.reduce((acc, num, index) => {
+      if (num >= target && acc === -1) {
+        return index;
+      }
+      return acc;
+    }, -1) || nums.length
+  );
 }
