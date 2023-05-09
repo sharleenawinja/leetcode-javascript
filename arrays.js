d3d2bebeb53da383bd2d5274f8a1cc69f19d9174
sharleenawinja <awinjasharleen@gmail.com>
2023-05-09 20:35:27 +0300

    reverse an array

--- arrays.js
+++ arrays.js
@@ -13,3 +13,22 @@ function findMaxElement(arr) {
 const nums = [4, 7, 2, 9, 1];
 const maxElement = findMaxElement(nums);
 console.log(maxElement); // Output: 9
+
+// Reverse an array in-place (without using any extra space).
+function reverseArray(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    const temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+  return arr;
+}
+
+// Example usage:
+const nums2 = [1, 2, 3, 4, 5];
+const reversedArray = reverseArray(nums);
+console.log(reversedArray); // Output: [5, 4, 3, 2, 1]
