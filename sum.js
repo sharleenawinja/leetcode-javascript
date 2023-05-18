a495712d4b5f46f9fa0c8ad7ebdfe4ec9226d395
sharleenawinja <awinjasharleen@gmail.com>
2023-05-18 15:43:36 +0300

    decode

--- sum.js
+++ sum.js
@@ -154,3 +154,27 @@ function uniquePaths(m, n) {
 
   return dp[m - 1][n - 1];
 }
+
+// Decode Ways
+function numDecodings(s) {
+  const n = s.length;
+  const dp = new Array(n + 1).fill(0);
+
+  dp[0] = 1;
+  dp[1] = s[0] === "0" ? 0 : 1;
+
+  for (let i = 2; i <= n; i++) {
+    const oneDigit = Number(s.slice(i - 1, i));
+    const twoDigits = Number(s.slice(i - 2, i));
+
+    if (oneDigit >= 1 && oneDigit <= 9) {
+      dp[i] += dp[i - 1];
+    }
+
+    if (twoDigits >= 10 && twoDigits <= 26) {
+      dp[i] += dp[i - 2];
+    }
+  }
+
+  return dp[n];
+}
