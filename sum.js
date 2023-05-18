b1f399f565bef6c36f5554f4f27d3fe07f5c6b0f
sharleenawinja <awinjasharleen@gmail.com>
2023-05-18 15:39:56 +0300

    longest substring

--- sum.js
+++ sum.js
@@ -99,3 +99,21 @@ const wait = (milliseconds) => {
 wait(1000).then(() => {
   console.log("One second has elapsed.");
 });
+
+// Find Longest Substring Without Repeating Characters
+function lengthOfLongestSubstring(s) {
+  let maxLength = 0;
+  let start = 0;
+  const charMap = new Map();
+
+  for (let end = 0; end < s.length; end++) {
+    const currentChar = s[end];
+    if (charMap.has(currentChar) && charMap.get(currentChar) >= start) {
+      start = charMap.get(currentChar) + 1;
+    }
+    charMap.set(currentChar, end);
+    maxLength = Math.max(maxLength, end - start + 1);
+  }
+
+  return maxLength;
+}
