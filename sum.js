c2b038f23161eac7939710d8400b6390db9660d1
sharleenawinja <awinjasharleen@gmail.com>
2023-05-18 15:44:16 +0300

    rotate image

--- sum.js
+++ sum.js
@@ -178,3 +178,20 @@ function numDecodings(s) {
 
   return dp[n];
 }
+
+//  Rotate Image
+function rotate(matrix) {
+  const n = matrix.length;
+
+  // Transpose the matrix
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+
+  // Reverse each row
+  for (let i = 0; i < n; i++) {
+    matrix[i].reverse();
+  }
+}
