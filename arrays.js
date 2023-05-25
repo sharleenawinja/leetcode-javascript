58be0842ee55cdc9929765cbcc9dca781fef4f24
sharleenawinja <awinjasharleen@gmail.com>
2023-05-25 19:55:12 +0300

    remove duplicates

--- arrays.js
+++ arrays.js
@@ -127,3 +127,31 @@ function isPalindrome(x) {
 }
 
 console.log(isPalindrome(121));
+
+// Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+
+// Example:
+// Input: nums = [1, 1, 2]
+// Output: 2
+// Explanation: After removing the duplicates, the array is [1, 2] and the length is 2.
+
+function removeDuplicates(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+
+  let uniqueCount = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[uniqueCount] = nums[i];
+      uniqueCount++;
+    }
+  }
+
+  return uniqueCount;
+}
+
+const num = [1, 1, 2];
+console.log(removeDuplicates(num));
+console.log(num);
