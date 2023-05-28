06bf5f90ed780cdf374b0ea17b3ce50c65672d3a
sharleenawinja <awinjasharleen@gmail.com>
2023-05-28 20:05:55 +0300

    majority element

--- arrays.js
+++ arrays.js
@@ -235,3 +235,31 @@ function climbStairs(n) {
 
 // Test case
 console.log(climbStairs(4)); // Output: 5
+
+// Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example:
+// Input: nums = [2, 2, 1, 1, 1, 2, 2]
+// Output: 2
+
+function majorityElement(nums) {
+  let majority = nums[0];
+  let count = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === majority) {
+      count++;
+    } else {
+      count--;
+      if (count === 0) {
+        majority = nums[i];
+        count = 1;
+      }
+    }
+  }
+
+  return majority;
+}
+
+// Test case
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // Output: 2
