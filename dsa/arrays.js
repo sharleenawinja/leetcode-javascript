3410e00d6a4254e41709b3e3e5df6339f90bacd7
sharleenawinja <awinjasharleen@gmail.com>
2023-06-17 16:30:32 +0300

    Longest Consecutive Sequence

--- dsa/arrays.js
+++ dsa/arrays.js
@@ -392,3 +392,36 @@ function subarraySum(nums, k) {
 
 // Test case
 console.log(subarraySum([1, 1, 1], 2)); // Output: 2
+
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// Example:
+// Input: nums = [100, 4, 200, 1, 3, 2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4].
+
+// Solution:
+
+function longestConsecutive(nums) {
+  const set = new Set(nums);
+  let longestStreak = 0;
+
+  for (let num of set) {
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+
+      while (set.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+
+      longestStreak = Math.max(longestStreak, currentStreak);
+    }
+  }
+
+  return longestStreak;
+}
+
+// Test case
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // Output: 4
