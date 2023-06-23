2dd1696e92aaa83fd2e950d3d11e1521abe4d19e
sharleenawinja <awinjasharleen@gmail.com>
2023-06-23 14:58:48 +0300

    valid parentheses

--- sum.js
+++ sum.js
@@ -223,3 +223,39 @@ function lengthOfLongestSubstring(s) {
 
 // Test case
 console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+// - Open brackets must be closed by the same type of brackets.
+// - Open brackets must be closed in the correct order.
+
+// Example:
+// Input: s = "()[]{}"
+// Output: true
+
+function isValid(s) {
+  const stack = [];
+  const brackets = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (brackets[char]) {
+      stack.push(char);
+    } else {
+      if (stack.length === 0 || brackets[stack.pop()] !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+// Test case
+console.log(isValid("()[]{}")); // Output: true
