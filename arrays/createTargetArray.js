4842059839c45870ee7b1c6b2f5af67033ac9053
sharleenawinja <awinjasharleen@gmail.com>
2023-07-07 11:21:42 +0300

    create target array

--- arrays/createTargetArray.js
+++ arrays/createTargetArray.js
@@ -43,3 +43,29 @@ const createTargetArray = (nums, index) => {
 console.log(createTargetArray([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]));
 console.log(createTargetArray([1, 2, 3, 4, 0], [0, 1, 2, 3, 0]));
 console.log(createTargetArray([1], [0]));
+
+// second approach using reduce
+const createTarget = (nums, index) => {
+  const targetArray = index.reduce(
+    (acc, cur, index) => (acc.splice(cur, 0, nums[index]), acc),
+    []
+  );
+  return targetArray;
+};
+
+console.log(createTarget([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]));
+console.log(createTarget([1, 2, 3, 4, 0], [0, 1, 2, 3, 0]));
+console.log(createTarget([1], [0]));
+
+// third approach
+const target = (nums, index) => {
+  let target = [];
+  for (const i in nums) {
+    target.splice(index[i], 0, nums[i]);
+  }
+  return target;
+};
+
+console.log(target([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]));
+console.log(target([1, 2, 3, 4, 0], [0, 1, 2, 3, 0]));
+console.log(target([1], [0]));
